c6ddce225e5456ce87b3b74df91057e8cba7151d
n0sc3tipsum <da322@ic.ac.uk>
2023-11-28 15:31:18 +0400

    Changed control decode to comply with v2 design
    Now the control signal decoder takes in a custom type of instruction and instruction sub type, and outputs the __Src control signals. Previously it determined the type of instruction and the AluCtrl signal - this is now performed by the instruction decoder and control path.

--- Control/Decoders/ControlDecode.sv
+++ Control/Decoders/ControlDecode.sv
@@ -1,118 +1,64 @@
 module ControlDecode(
-  input  logic [7:1] iOpCode,
-  input  logic [3:1] iFunct3,
-  input  logic [7:1] iFunct7,
+  input InstructionTypes    iInstructionType,
+  input InstructionSubTypes iInstructionSubType,
 
-  output logic [4:1] oAluCtrl
-  output logic [2:1] oTypeI,
-  output logic [2:1] oTypeU,
-  output logic [2:1] oTypeJ,
-  output logic       oTypeS,
-  output logic       oTypeR,
-  output logic       oTypeB,
+  output logic              oResultSrc,
+  output logic              oPCSrc,
+  output logic              oAluSrc,
+  output logic              oRegWrite,
+  output logic              oMemWrite
 );
 
-/*
-TO DO : 
+initial begin
+  assign oRegWrite   = 1'b0;
+  assign oAluSrc     = 1'b0;
+  assign oPCSrc      = 1'b0;
+  assign oResultSrc  = 1'b0;
+  assign oMemWrite   = 1'b0;
+end
 
 
-*/
+always_comb begin
 
-////////////////////////////
-///Internal Logic Signals///
-////////////////////////////
+  case(iInstructionType)
 
-  logic [9:1] instruction_types;
+    REG_COMMPUTATION : oRegWrite = 1'b1;
 
-  logic flag_ldr;
-  logic flag_str;
-  logic flag_r_type;
-  logic flag_reg_imm_comp;
-  logic flag_shift_right;
-  logic flag_arithmetic;
+    IMM_COMPUTATION  : begin
+      oRegWrite = 1'b1;
+      oAluSrc   = 1'b1;
+    end
 
-  logic [3:1] load_types;
-  logic [2:1] uload_types;
-  logic [3:1] store_types;
-  logic [8:1] computation_types;
-  logic [2:1] arithemetic_types;
-  logic [2:1] right_shift_types;
+    LOAD   : begin
+      oRegWrite  = 1'b1;
+      oResultSrc = 1'b1;
+    end
 
+    UPPER  : begin
+      if (iInstructionSubType == LOAD_UPPER_IMM) begin
+        oRegWrite = 1'b1;
+        oAluSrc   = 1'b1;
+      end
 
-////////////////////////////////////////
-///First Stage of Decoders : OpDecode///
-////////////////////////////////////////
+      else begin
+        oPCSrc = 1'b1;
+      end
+    end
 
-  op_decode OpDecode(
-    .OpCode(OpCode),
-    .oInstructionType(instruction_types)
-  );
+    STORE  : begin
+      oMemWrite = 1'b1;
+      oAluSrc = 1'b1;
+    end
 
-  always_comb begin
-    TypeB = instruction_types[9];
-    TypeJ = instruction_types[8:7];
-    TypeR = instruction_types[6];
-    TypeU = instruction_types[5:4];
-    TypeI = instruction_types[3:2];
-    TypeS = instruction_types[1];
+    JUMP   : begin
+      oPCSrc = 1'b1;
+      oRegWrite = 1'b1; //Since we write PC+4 to destination for JAL
+    end
 
-    flag_reg_imm_comp = instruction_types[3];
-    flag_r_type       = instruction_types[6];
-    flag_ldr          = instruction_types[2];
-    flag_str          = instruction_types[1];
-  end
+    // TO DO : add control logic to check if PC src is 1 for branch instruction given the iZero input
 
+  endcase
+end
 
-//////////////////////////////////////////////////////////
-///Second Stage of Decoders : Funct3 and Func7 Decoders///
-//////////////////////////////////////////////////////////
-
-  Funct3Decode Funct3Decoder(
-    .iFunct3(iFunct3),
-    .iLoad(flag_ldr),
-    .iStore(flag_str),
-    .iTypeR(flag_r_type),
-    .iRegImmComputation(flag_reg_imm_comp),
-
-    .oLoadBHW(load_types),
-    .oULoadBH(uload_types),
-    .oStoreBHW(store_types),
-    .oComputationType(computation_types)
-  );
-
-  Funct7Decode Funct7Decoder(
-  
-    .iArithmeticIns(flag_arithmetic),
-    .iShiftRightIns(flag_shift_right),
-    .iFunct7(iFunct7),
-
-    .oArithmeticType(arithemetic_types),
-    .oRightShiftType(right_shift_types)
-  );
-
-
-////////////////////////////////////////////////
-///ALU Control Encoder - Generates AluControl///
-////////////////////////////////////////////////
-
-  logic [9:1] alu_message;
-
-  //Fill in the alu message you want to encode
-  always_comb begin
-    alu_message[9:8] = right_shift_types;     //{arithmetic, logical}
-    alu_message[7]   = computation_types[1];  //left shift
-    alu_message[6]   = computation_types[8];  //and
-    alu_message[5]   = computation_types[5];  //xor
-    alu_message[4:3] = arithemetic_types;     //{sub, add}
-    alu_message[2:1] = computation_types[4:3] //{usetless, setless}
-  end
-
-  ALU_encode AluEncoder(
-    .iAluOps(AluMessage),
-    .iTypeB(instruction_types[9]),
-    .iTypeS(instruction_types[1]),
-
-    .oAluCtrl(AluCtrl)
-  );
 
 endmodule
