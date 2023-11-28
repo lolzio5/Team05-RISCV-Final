d1209f29cbb4698781d19419e5ec70b44e06314d
n0sc3tipsum <da322@ic.ac.uk>
2023-11-29 00:27:44 +0400

    Added commments and formed the main control unit top sheet
    This updated control unit top sheet (v2) incorporates the instruction ROM and control path to produce the full set of control signals as required from the control unit.

--- Control/ControlMain.sv
+++ Control/ControlMain.sv
@@ -2,52 +2,64 @@ module ControlMain(
   input logic [32:1]  iPC,
   input logic         iZero,
 
-  output logic [32:1] oImmOpp,
+
+  output logic [32:1] oImmExt,
   output logic        oRegWrite,
   output logic        oMemWrite,
+  output logic [4:1]  oMemControl
 
-  output logic [4:1]  oAluctrl,
+  output logic [4:1]  oAluControl,
   output logic        oAlusrc,
+  
   output logic        oImmSrc,
-  output logic        oPCsrc
+  output logic        oPCsrc,
+  output logic        oResultSrc,
 
-  output logic [5:1]  ors1,
-  output logic [5:1]  ors2,
-  output logic [5:1]  ord,
+  output logic [5:1]  oRs1,
+  output logic [5:1]  oRs2,
+  output logic [5:1]  oRd,
 );
+
+
+////////////////////////////////
+////      Internal Logic    ////
+////////////////////////////////
 
   logic [32:1] current_instruction;
 
-  logic [20:1] imm20;
-  logic [32:1] imm32;
+
+
+////////////////////////////////
+//// Instruction Memory ROM ////
+////////////////////////////////
 
   ROM InstrMem(
     .iPC(iPC),
     .oInstr(current_instruction)
   );
 
+
+
+////////////////////////////////
+////      Control Path      ////
+////////////////////////////////
+
   ControlPath ControlPath(
-    .oInstruction(current_instruction),
+    .iInstruction(current_instruction),
+    .iZero(iZero),
 
-    .oAluCtrl(oAluctrl),
-    .oImmSrc(imm20),
-    .oAluSrc(oAlusrc),
-    .oRegWrite(oRegWrite)
+    .oAluControl(oAluControl),
+    .oImmExt(oImmExt),
+    .oAluSrc(oAluSrc),
+    .oPCSrc(oPCSrc),
+    .oResultSrc(oResultSrc),
+    .oMemControl(oMemControl),
+    .oMemWrite(oMemWrite),
+    .oRegWrite(oRegWrite),
+    .oRs1(oRs1),
+    .oRs2(oRs2),
+    .oRd(oRd)
   );
-
-  SignExtend SignExtender(
-    .iImm20(imm20),
-    .oImm32(oImmOp)
-  );
-
-  always_comb begin
-
-    //TO DO : Add logic to determine PC src
-
-    rs1 = curr_ins[20:16];
-    rs2 = curr_ins[25:21];
-    rd  = curr_ins[12:8];
-  end
 
 endmodule
 
