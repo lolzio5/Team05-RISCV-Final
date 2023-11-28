013222c61fe8f18d3499e880665b8ab5db4a470d
n0sc3tipsum <da322@ic.ac.uk>
2023-11-28 15:41:20 +0400

    Changed the Control Main sheet to accomadate the data bus IO of the updated components (v1)

--- Control/ControlMain.sv
+++ Control/ControlMain.sv
@@ -1,49 +1,53 @@
 module ControlMain(
-  input logic [32:1]  PC,
-  input logic         EQ,
+  input logic [32:1]  iPC,
+  input logic         iZero,
 
-  output logic [32:1] ImmOpp,
-  output logic        RegWrite,
-  output logic [4:1]  Aluctrl,
-  output logic        Alusrc,
-  output logic [5:1]  rs1,
-  output logic [5:1]  rs2,
-  output logic [5:1]  rd,
-  output logic        ImmSrc,
-  output logic        PCsrc
+  output logic [32:1] oImmOpp,
+  output logic        oRegWrite,
+  output logic        oMemWrite,
+
+  output logic [4:1]  oAluctrl,
+  output logic        oAlusrc,
+  output logic        oImmSrc,
+  output logic        oPCsrc
+
+  output logic [5:1]  ors1,
+  output logic [5:1]  ors2,
+  output logic [5:1]  ord,
 );
 
-logic [32:1] curr_ins;
+  logic [32:1] current_instruction;
 
-logic [12:1] imm12;
-logic [32:1] imm32;
+  logic [20:1] imm20;
+  logic [32:1] imm32;
 
-ROM InstrMem(
-  .PC(PC),
-  .Instr(curr_ins)
-);
+  ROM InstrMem(
+    .iPC(iPC),
+    .oInstr(current_instruction)
+  );
 
-ControlPath ControlPath(
-  .instruction(curr_ins),
+  ControlPath ControlPath(
+    .oInstruction(current_instruction),
 
-  .AluCtrl(Aluctrl),
-  .ImmSrc(imm12),
-  .AluSrc(Alusrc),
-  .RegWrite(RegWrite)
-);
+    .oAluCtrl(oAluctrl),
+    .oImmSrc(imm20),
+    .oAluSrc(oAlusrc),
+    .oRegWrite(oRegWrite)
+  );
 
-SignExtend SignExtender(
-  .imm_12(imm12),
-  .imm_32ext(imm32)
-);
+  SignExtend SignExtender(
+    .iImm20(imm20),
+    .oImm32(oImmOp)
+  );
 
-always_comb begin
-  ImmOpp = imm32;
-  PCsrc = EQ ;
-  rs1 = curr_ins[20:16];
-  rs2 = curr_ins[25:21];
-  rd  = curr_ins[12:8];
-end
+  always_comb begin
+
+    //TO DO : Add logic to determine PC src
+
+    rs1 = curr_ins[20:16];
+    rs2 = curr_ins[25:21];
+    rd  = curr_ins[12:8];
+  end
 
 endmodule
 
