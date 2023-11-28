11a5e6fe025a320a7f1bd9d5ed5cdd24c5e11379
n0sc3tipsum <da322@ic.ac.uk>
2023-11-29 00:12:20 +0400

    Changed IO naming of ROM

--- Control/Memory/ROM.sv
+++ Control/Memory/ROM.sv
@@ -4,23 +4,19 @@ module ROM #(
             DATA_WIDTH = 32 
 
 )(
-
-  input  logic [ADDR_WIDTH-1:0] PC,
-  output logic [DATA_WIDTH-1:0] Instr
-
+  input  logic [ADDR_WIDTH-1:0] iPC,
+  output logic [DATA_WIDTH-1:0] oInstruction
 );
 
 logic [DATA_WIDTH-1:0] rom_array [2**ADDR_WIDTH-1:0];
 
 initial begin
-
         $display("Loading ROM");
         $readmemh("Instructions.mem", rom_array);
-
 end;
 
 always_comb
 
-  Instr = rom_array[PC];
+  oInstruction = rom_array[PC];
 
 endmodule
