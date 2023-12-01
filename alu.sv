2e4d7a8548d57eb146c6b3e8eb9857584b2a7b51
songmeric <77728557+songmeric@users.noreply.github.com>
2023-12-01 11:22:22 +0000

    Update alu.sv

--- alu.sv
+++ alu.sv
@@ -1,36 +1,35 @@
-
 module alu #(
     parameter  OP_WIDTH = 4,
                DATA_WIDTH =32
 )(
-    input logic [OP_WIDTH-1:0] ALUControl,
-    input logic [DATA_WIDTH-1:0]      SrcA,
-    input logic [DATA_WIDTH-1:0]      SrcB,
-    output logic [DATA_WIDTH-1:0]     ALUResult,
-    output logic                      Zero
+    input logic [OP_WIDTH-1:0] ALUControl /*verilator public*/,
+    input logic [DATA_WIDTH-1:0]      SrcA /*verilator public*/,
+    input logic [DATA_WIDTH-1:0]      SrcB /*verilator public*/,
+    output logic [DATA_WIDTH-1:0]     ALUResult /*verilator public*/,
+    output logic                      Zero 
 );
-    logic out [DATA_WIDTH-1:0];
+    logic [DATA_WIDTH-1:0] out;
     always_comb begin
-        localparam int ALUOP_NOP = OPERATION_WIDTH'd0;
+        ALUResult = {DATA_WIDTH{1'b0}};
         case (ALUControl)
-            OP_WIDTH'b0000: out = SrcA+SrcB; //Addition
-            OP_WIDTH'b0001: out = SrcA-SrcB; //Subtraction
-            OP_WIDTH'b0010: out = SrcA<<SrcB; //Left shift
-            OP_WIDTH'b0011: out = ($signed(SrcA) > $signed(SrcB)) ? 0 : 1; //Set less then
-            OP_WIDTH'b0100: out = ($unsigned(SrcA) > $unsigned(SrcB)) ? 0 : 1; //Unsigned set less than
-            OP_WIDTH'b0101: out = SrcA ^ SrcB; //XOR
-            OP_WIDTH'b0110: out = SrcA>>SrcB; //Right shift logical
-            OP_WIDTH'b0111: out = SrcA>>>SrcB; //Right shift arithmetic
-            OP_WIDTH'b1000: out = SrcA | SrcA; //OR
-            OP_WIDTH'b1001: out = SrcA & SrcB; //AND
-            default: out = 0;
+            4'b0000: out = SrcA+SrcB; //Addition
+            4'b0001: out = SrcA-SrcB; //Subtraction
+            4'b0010: out = SrcA<<SrcB; //Left shift
+            4'b0011: out = ($signed(SrcA) > $signed(SrcB)) ? 0 : 1; //Set less then
+            4'b0100: out = ($unsigned(SrcA) > $unsigned(SrcB)) ? 0 : 1; //Unsigned set less than
+            4'b0101: out = SrcA ^ SrcB; //XOR
+            4'b0110: out = SrcA>>SrcB; //Right shift logical
+            4'b0111: out = SrcA>>>SrcB; //Right shift arithmetic
+            4'b1000: out = SrcA | SrcA; //OR
+            4'b1001: out = SrcA & SrcB; //AND
+            default: out = 32'd5;
         endcase
-        if(out==DATA_WIDTH'b0) begin
-            Zero <=1'b1;
-            ALUResult <= 1'b0;
-        end
-        else begin
-            ALUResult <=out;
-        end
+        //if(out==32'b0) begin
+          //  Zero =1'b1;
+            //ALUResult = 32'b0;
+        //end
+        //else begin
+            ALUResult = out;
+        //end
     end
 endmodule
