cdd3fa6e475c8d8bcc18cd954e76f98e129d2014
n0sc3tipsum <da322@ic.ac.uk>
2023-12-02 23:50:32 +0400

    Update ImmDecode.sv
    - Changed immediate operand bit assignment to go from lsb to msb so that sign extension is possible
    - Removed old comments

--- Control/ImmDecode.sv
+++ Control/ImmDecode.sv
@@ -1,4 +1,4 @@
-`include "ControlTypeDefs.svh"
+`include "include/ControlTypeDefs.svh"
 
 module ImmDecode(
   input  InstructionTypes    iInstructionType,
@@ -9,7 +9,7 @@ module ImmDecode(
   output logic [31:0]      oImmExt
 );
 
-//Change on 30th nov : Made ImmDecode output the sign extended immediate 
+
 
 /*Note on jumps
 
@@ -20,68 +20,57 @@ Similarly, AUIPC then JALR can jump anywhere in a 32-bit pc-relative address ran
 
 */
 
-/*
-TO DO : 
-  - change the immediate for B and J type so that LSB is = 0 (according to RISCV spec)
-  - The jump type instruction encodes the immediate in multiples of 2 - bytes (implement this )
-  - JALR Uses the i-type encoding of the immediate : The target address 
-    is found by adding this immedaite to rs1 and setting LSB of result to 0
-
-  
-*/
-
 always_comb begin
 
-
-    oImmExt = {32{1'b0}};
+  //Init Output
+  oImmExt = {32{1'b0}};
 
   case(iInstructionType)
   
     IMM_COMPUTATION : begin      
-      oImmExt[31:12] = {20{oImmExt[11]}};
       oImmExt[11:0]  = iInstruction[31:20];
+      oImmExt[31:12] = {20{oImmExt[11]}};
     end
 
     LOAD : begin      
-      oImmExt[31:12] = {20{oImmExt[11]}};
       oImmExt[11:0]  = iInstruction[31:20];
+      oImmExt[31:12] = {20{oImmExt[11]}};
     end
 
     UPPER : begin
-      oImmExt[31:12] = iInstruction[31:12];
       oImmExt[11:0]  = {12{1'b0}};
+      oImmExt[31:12] = iInstruction[31:12];
     end
  
     STORE : begin      
-      oImmExt[31:12] = {20{oImmExt[11]}};
-      oImmExt[11:5]  = iInstruction[31:25];
       oImmExt[4:0]   = iInstruction[11:7];
+      oImmExt[11:5]  = iInstruction[31:25];
+      oImmExt[31:12] = {20{oImmExt[11]}};
     end
 
     JUMP : begin
       if(iInstructionSubType == JUMP_LINK_REG) begin        
-        oImmExt[31:12] = {20{oImmExt[11]}};
         oImmExt[11:0]  = iInstruction[31:20];
+        oImmExt[31:12] = {20{oImmExt[11]}};
       end
 
       else begin
-        oImmExt[31:20]  = {12{iInstruction[31]}};
-        oImmExt[19:12]  = iInstruction[19:12];
-        oImmExt[11]     = iInstruction[20];
-        oImmExt[10:1]   = iInstruction[30:21];
-        oImmExt[0]      = 1'b0; 
+        oImmExt[1:0]    = 2'b00; 
+        oImmExt[11:2]   = iInstruction[30:21];
+        oImmExt[12]     = iInstruction[20];
+        oImmExt[20:13]  = iInstruction[19:12];
+        oImmExt[31:21]  = {11{iInstruction[31]}};
       end
     end
 
     BRANCH : begin      
-      oImmExt[31:13] = {19{oImmExt[11]}};       
-      oImmExt[12:1]  = {iInstruction[31], iInstruction[7], iInstruction[30:25], iInstruction[11:8]};
-      oImmExt[0]     = 1'b0; //Branch immediate encoded in multiples of 2 (lsb = 0)
+      oImmExt[1:0]   = 2'b00; //Branch immediate encoded in multiples of 4 (lsb = 0)
+      oImmExt[13:2]  = {iInstruction[31], iInstruction[7], iInstruction[30:25], iInstruction[11:8]};
+      oImmExt[31:14] = {18{oImmExt[11]}};
     end
 
-    default : begin
-      oImmExt = {32{1'b0}}; //set imm20 to 0 by default
-    end
+    default : oImmExt = {32{1'b0}}; //set imm20 to 0 by default
+    
   endcase
 end
 
