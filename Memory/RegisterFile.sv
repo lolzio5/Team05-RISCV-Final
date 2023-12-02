c08823386b247acc3c3c066eea5941746de008f2
n0sc3tipsum <da322@ic.ac.uk>
2023-12-02 23:14:52 +0400

    Updated Register File
    - Renamed register to RegisterFile
    - Updated naming and style to follow general style
    - Updated logic so that we can read and write registers simoultaneously
    - Changed 'initial' statement to 'always_comb' so that zero register is always set to 0

---
+++ Memory/RegisterFile.sv
@@ -0,0 +1,37 @@
+module RegisterFile #(
+    parameter  ADDRESS_WIDTH = 5,
+               DATA_WIDTH = 32
+)(
+    input  logic iClk,
+    input  logic iWriteEn,
+    input  logic [ADDRESS_WIDTH-1:0] iReadAddress1,
+    input  logic [ADDRESS_WIDTH-1:0] iReadAddress2,
+    input  logic [ADDRESS_WIDTH-1:0] iWriteAddress,
+    input  logic [DATA_WIDTH-1:0]    iDataIn,  
+
+    output logic [DATA_WIDTH-1:0]    oRegData1,
+    output logic [DATA_WIDTH-1:0]    oRegData2 
+);
+
+    logic [DATA_WIDTH-1:0] ram_array [0:2**ADDRESS_WIDTH-1];
+
+    always_comb begin
+        ram_array[0] = {DATA_WIDTH{1'b0}};
+    end
+
+    always_ff @ (posedge iClk ) begin
+
+        if(iWriteEn == 1'b1) begin
+            ram_array[iWriteAddress] <= iDataIn;
+            oRegData1 <= ram_array[iReadAddress1]; //still need to output register data if we are writing
+            oRegData2 <= ram_array[iReadAddress2];
+        end
+        
+        else begin
+            oRegData1 <= ram_array[iReadAddress1];
+            oRegData2 <= ram_array[iReadAddress2];
+        end
+
+    end
+
+endmodule
