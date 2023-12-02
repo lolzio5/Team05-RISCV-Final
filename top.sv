9915ac85af6646d7778d8a9f9c920b7e83834bc8
n0sc3tipsum <da322@ic.ac.uk>
2023-12-02 23:54:24 +0400

    Updated top.sv
    - Changed naming and style to follow general style
    - Re-organized module structure
    - Added in the updated modules
    - Removed unneeded components like mux
    - Updated control and data signals

--- top.sv
+++ top.sv
@@ -1,87 +1,195 @@
-`include "Control/ControlTypeDefs.svh"
-
+`include "include/ControlTypeDefs.svh"
 
 module top(
-    input  logic         clk,         // Clock input
-    input  logic         rst, 
-    input  logic         trigger,
-    output logic [31:0]  data_out  // Output data
+    input  logic         iClk,         // Clock input
+    input  logic         iRst, 
+    //input  logic         iTrigger, commented out until implemented
+    output logic [31:0]  oDataOut // Output data
 );
-    logic [31:0] MemWrite;
-    logic [31:0] rd;
-    logic [31:0] rs1;
-    logic [31:0] rs2;
-    logic [31:0] ALUctrl;
-    logic [31:0] ALUsrc;
-    logic [31:0] rs1;
-    logic [31:0] rs2;
-    logic [31:0] ImmExt;
-    logic [31:0] PC;
-    logic [31:0] ResultSrc;
-    logic [31:0] WriteData;
-    logic [31:0] ALUResult;
 
-    pcreg myPcreg(
-        .clk (clk),
-        .rst (rst),
-        .PCsrc (PCSrc)
-        .ImmExt (ImmExt),
-        .PC (PC)
+
+////////////////////////////////
+////      PC Register       ////
+////////////////////////////////
+
+
+    logic [31:0] pc;
+    logic [31:0] target_pc;
+    logic        pc_src;
+
+    PCRegister PCRegister(
+        .iClk(iClk),
+        .iRst(iRst),
+        .iPCSrc(pc_src),
+        .iTargetPC(target_pc),
+        .oPC(pc)
     );
 
-    datamem myDatamem(
-        .clk (clk),
-        .ResultSrc (ResultSrc),
-        .WE (MemWrite),
-        .A (ALUResult),
-        .WD (WriteData),
-        .Result (WD3)
+
+//////////////////////////////////////////////////////////
+//// Control Unit : Control Path + Instruction Memory ////
+//////////////////////////////////////////////////////////
+
+    /* [ Current Instruction Type ]           
+       -- Output by Control Unit -- */
+    InstructionSubTypes instruction_type;
+
+
+    /* [ Source and Destination Register Addesses ]           
+       -- Output by Control Unit -- */
+    logic [4:0]  rs1;
+    logic [4:0]  rs2;
+    logic [4:0]  rd;
+
+
+    /* [ Immedaite Operand ]           
+       -- Output by Control Unit -- */
+    logic [31:0] imm_ext;
+
+
+    /* [ Zero Flag ]           
+       -- Output by Control Unit -- */
+    logic        zero;
+
+    ControlUnit ControlUnit(
+        .iPC(pc),
+        .iZero(zero), 
+        .oImmExt(imm_ext),
+        .oRegWrite(reg_write_en),
+        .oMemWrite(mem_write_en),
+        .oAluControl(alu_control),
+        .oAluSrc(alu_src),
+        .oResultSrc(result_src),
+        .oPCSrc(pc_src),
+        .oRs1(rs1),
+        .oRs2(rs2),
+        .oRd(rd),
+        .oInstructionSubType(instruction_type)
     );
 
-    alu myALU(
-        .ALUControl (ALUctrl),
-        .SrcA (rs1),
-        .SrcB (rs2),
-        .ALUResult (ALUResult),
-        .Zero (Zero)
+
+/////////////////////////////////
+////      Register File      ////
+/////////////////////////////////
+
+
+    //Register Data Out (Read Data) -     Output From Register File
+    logic [31:0] reg_data_out1;
+    logic [31:0] reg_data_out2;
+
+
+    //Register Data In (Write Data) -     Output From ResultMux 
+    logic [31:0] reg_data_in;
+
+
+    //Control Signals               -     Output From control Unit
+    logic        reg_write_en;
+
+    RegisterFile RegisterFile(
+        .iClk(iClk),
+        .iWriteEn(reg_write_en),
+        .iReadAddress1(rs1),
+        .iReadAddress2(rs2),
+        .iWriteAddress(rd),
+        .iDataIn(reg_data_in),
+        .oRegData1(reg_data_out1),
+        .oRegData2(reg_data_out2)
     );
 
-    mux myMux1(
-        .iselect (ALUsrc),
-        .iSrcA (RD2),
-        .iSrcB (ImmExt),
-        .oselected (SrcB)
-    );
-    mux myMux2(
-        .iselect (ResultSrc),
-        .iSrcA (ALUResult),
-        .iSrcB (WD3),
-        .oselected (Result)
+
+/////////////////////////////////
+////      Data Memory        ////
+/////////////////////////////////
+
+    //Memory Data I/O 
+    logic [31:0] mem_data_in;
+    logic [31:0] mem_data_out;
+
+
+    //Control Signals     -   Output From Control Unit
+    logic        mem_write_en;
+
+
+    // Data that is to be written to data memory is the value stored in the second source register
+    always_comb begin
+        mem_data_in = reg_data_out2;
+    end
+
+    DataMemory DataMemory(
+        .iClk(iClk),
+        .iWriteEn(mem_write_en),
+        .iMemoryInstructionType(instruction_type), 
+        .iAddress(alu_result),
+        .iMemData(mem_data_in),
+        .oMemData(mem_data_out)
     );
 
-    register myRegister(
-        .CLK (clk),
-        .WE3 (RegWrite),
-        .A1 (rs1),
-        .A2 (rs2),
-        .A3 (rd),
-        .WD3 (Result),
-        .RD1 (SrcA),
-        .RD2 (SrcB)
+
+/////////////////////////////////
+////  Arithmetic Logic Unit  ////
+/////////////////////////////////
+
+
+    //Data Signal       -     Output From Alu
+    logic [31:0] alu_result;
+
+
+    //Alu Operands      -     Otput From Register File and Control Unit (Immediate Operand)
+    logic [31:0] alu_op1;
+    logic [31:0] alu_op2;
+
+
+    //Control Signals   -     Output From Control Unit
+    logic [3:0]  alu_control;
+    logic        alu_src;
+
+
+    always_comb begin
+        alu_op1 = reg_data_out1;
+        alu_op2 = alu_src ? imm_ext : reg_data_out2 ; //Pick between immediate or register operand
+    end
+    
+    Alu Alu(
+        .iAluControl(alu_control),
+        .iAluOp1(alu_op1),
+        .iAluOp2(alu_op2),
+        .oAluResult(alu_result),
+        .oZero(zero)
+    );    
+
+///////////////////////////////////////////
+////  PC Adder : Target PC Calculator  ////
+///////////////////////////////////////////
+
+
+    PCAdder TargetPCAdder(
+        .iPC(pc),
+        .iImmExt(imm_ext),
+        .iInstructionSubType(instruction_type),
+        .iRegOffset(alu_op1),
+        .oPCTarget(target_pc)
     );
 
-    ControlMain myControlMain(
-        .iPC (PC),
-        .iZero (Zero), 
-        .oImmOpp (ImmExt),
-        .oRegWrite (RegWrite),
-        .oMemWrite (MemWrite)
-        .oAluctrl (ALUctrl),
-        .oAlusrc (ALUsrc),
-        .ors1 (rs1),
-        .ors2 (rs2),
-        .ord (rd),
-    );
+//////////////////////////////////////////////////////////////////////////////////////
+////  Write Back Result Selector : Choses What Is Written Back To Register File  /////
+//////////////////////////////////////////////////////////////////////////////////////
 
+    //Control Signals   -   Output From Control Unit
+    logic [2:0]  result_src;
+
+    ResultMux ResultSelector(
+        .iResultSrc(result_src),
+        .iAluResult(alu_result),
+        .iMemDataOut(mem_data_out),
+        .iPC(pc),//
+        .iUpperImm(imm_ext),
+        .oRegDataIn(reg_data_in)
+    );  
+
+//Assign Output As Register Write Back Value
+
+    always_comb begin
+        oDataOut = reg_data_in;
+    end
 
 endmodule
