7ec42b56da41faebfb6ba8dadb7e18a9a041e253
n0sc3tipsum <da322@ic.ac.uk>
2023-12-02 23:24:01 +0400

    Updated AluEncode
    - Made it so that for jump and branch instructions the ALU control signal would be 0b1111 ('NULL' ie. Alu doesn't play a part)

--- Control/AluEncode.sv
+++ Control/AluEncode.sv
@@ -1,4 +1,4 @@
-`include "ControlTypeDefs.svh"
+`include "include/ControlTypeDefs.svh"
 
 module AluEncode(
   input InstructionTypes iInstructionType,
@@ -11,19 +11,14 @@ module AluEncode(
 
     case(iInstructionType)
 
-      REG_COMMPUTATION : oAluCtrl.REG_COMPUTATION  = iInstructionSubType.R;
+      REG_COMMPUTATION : oAluCtrl.REG_COMPUTATION  = iInstructionSubType.R; 
       IMM_COMPUTATION  : oAluCtrl.IMM_COMPUTATION  = iInstructionSubType.I;
-      STORE            : oAluCtrl.IMM_COMPUTATION  = IMM_ADD; //Might need to change this. Instead of ALU computing store/load/branch/jump address, use a seperate unit
+      STORE            : oAluCtrl.IMM_COMPUTATION  = IMM_ADD; 
       LOAD             : oAluCtrl.IMM_COMPUTATION  = IMM_ADD;
-      BRANCH           : oAluCtrl.REG_COMPUTATION  = SUB;
-      JUMP             : oAluCtrl.IMM_COMPUTATION  = IMM_ADD;
-
-      UPPER : begin
-        if (iInstructionSubType == LOAD_UPPER_IMM) oAluCtrl.IMM_COMPUTATION = IMM_ADD;
-        else                                       oAluCtrl.IMM_COMPUTATION = NULL_I;
-      end
-
-      default : oAluCtrl.NULL = 4'b1111;
+      BRANCH           : oAluCtrl.REG_COMPUTATION  = SUB;     //Has to be sub in order to compare registers
+      JUMP             : oAluCtrl.NULL = 4'b1111;
+      UPPER            : oAluCtrl.NULL = 4'b1111;
+      default          : oAluCtrl.NULL = 4'b1111;
     endcase
   end
 
