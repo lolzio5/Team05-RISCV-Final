324ed928fe42dd434c50dab0f42c24c0df511fb2
n0sc3tipsum <da322@ic.ac.uk>
2023-12-02 23:12:56 +0400

    Copied Muxs which were present in initial main design

---
+++ Mux/mux.sv
@@ -0,0 +1,18 @@
+module mux #(
+    parameter  ADDRESS_WIDTH = 5,
+               DATA_WIDTH =32
+)(
+    input logic iselect,
+    input logic [DATA_WIDTH-1:0] iSrcA,
+    input logic [DATA_WIDTH-1:0] iSrcB,
+    output logic [DATA_WIDTH-1:0] oselected
+);
+
+always_comb begin
+    if(iselect==1'b1)
+        oselected<=iSrcA;
+    else
+        oselected<=iSrcB;
+end
+
+endmodule
