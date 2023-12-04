1f4aa3c29779c6da720055d9c007a8a87a4e6180
n0sc3tipsum <da322@ic.ac.uk>
2023-12-04 21:13:37 +0400

    updated the CPU test bench
    This test bench doesn't require the vbuddy, instead it just uses the gtkwave

--- top_tb.cpp
+++ top_tb.cpp
@@ -9,40 +9,31 @@ int main(int argc, char **argv, char **env) {
     Verilated::commandArgs(argc, argv);
     // init top verilog instance
     Vtop* top = new Vtop;
+
     // init trace dump
     Verilated::traceEverOn(true);
     VerilatedVcdC* tfp = new VerilatedVcdC;
     top->trace (tfp, 99);
     tfp->open ("top.vcd");
- 
-    // init Vbuddy
-    if (vbdOpen()!=1) return(-1);
-    vbdHeader("Single Cycle CPU");
-    vbdSetMode(1);
 
     // initialize simulation input 
-    top->clk = 1;
-    top->rst = 1;
-    top->trigger = 1;
+    top->iClk = 1;
+    top->iRst = 0;
 
     // run simulation for MAX_SIM_CYC clock cycles
-    for (simcyc=0; simcyc<300; simcyc++) {
+    for (simcyc=0; simcyc<300; simcyc++) 
+    {
         // dump variables into VCD file and toggle clock
-        for (tick=0; tick<2; tick++) {
+        for (tick=0; tick<2; tick++) 
+        {
             tfp->dump (2*simcyc+tick);
-            top->clk = !top->clk;
+            top->iClk = !top->iClk;
             top->eval ();
         }
-        top->rst=0;
-
-        vbdBar(top->data_out);
-    
-        vbdCycle(simcyc);
 
         if (Verilated::gotFinish()) exit(0);
     }
 
-    vbdClose();
     tfp->close(); 
     printf("Exiting\n");
     exit(0);
