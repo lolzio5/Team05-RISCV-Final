2af18d9200f9f483e556aa815168b1048f24ec05
n0sc3tipsum <da322@ic.ac.uk>
2023-12-04 21:12:24 +0400

    Updated InstructionMemory.sv
    - Changed the way instructions are loaded into memory and then read out. Now the instruction word out of memory will have to be concatenated since each word in instruction memory is actually 8-bits due to byte addressable memory -> thus for a given address in memory given by iPC, you load the 8LSBs stored in the rom array starting at address iPC up to iPC+4 (to load 4 bytes of instruction)
    
    - Changed the depth of the rom to be the same as shown in the memory map -> this means that technically the address is 12 bits wide so only the 12 LSBs of iPC are used (trying to use a 32-bit address gave an out of bounds error)

--- Memory/InstructionMemory.sv
+++ Memory/InstructionMemory.sv
@@ -1,22 +1,25 @@
 module InstructionMemory #(
 
-  parameter ADDR_WIDTH = 32,
-            DATA_WIDTH = 32 
+  parameter DATA_WIDTH = 32
 
 )(
+   /* verilator lint_off UNUSED */
   input  logic [31:0] iPC,
+   /* verilator lint_on UNUSED */
   output logic [DATA_WIDTH-1:0] oInstruction
 );
 
-  logic [DATA_WIDTH-1:0] rom_array [2**ADDR_WIDTH-1:0];
+  logic [DATA_WIDTH - 1 : 0] rom_array [0 : 2**12  - 1];
 
   initial begin
           $display("Loading ROM");
-          $readmemb("InstructionsBin.mem", rom_array);
+          //$readmemb("InstructionsBin.mem", rom_array, 32'hBFC00000, 32'hBFC00FFF);
+          $readmemh("F1.s.hex", rom_array);
   end;
 
   always_comb begin
-    oInstruction = rom_array[iPC];
+
+    oInstruction = {rom_array[iPC + 32'd3][7:0], rom_array[iPC + 32'd2][7:0], rom_array[iPC + 32'd1][7:0], rom_array[iPC][7:0] };
   end
 
 endmodule
