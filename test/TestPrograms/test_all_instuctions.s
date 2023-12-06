d0903c01db99c206409e31130a37c6ca12df8071
samuelwbarber <sbarber9876@outlook.com>
2023-12-06 10:56:08 +0000

    fixing error

--- test/TestPrograms/test_all_instuctions.s
+++ test/TestPrograms/test_all_instuctions.s
@@ -1,16 +1,16 @@
-//Testing basic arithmatic:
+#Testing basic arithmatic:
 li s0, 0xF1
-addi s1, s0, 0x1 //Should output 0xF2
+addi s1, s0, 0x1 #Should output 0xF2
 
-xor s2, s1, s0 //Should ouput 0x2 
+xor s2, s1, s0 #Should ouput 0x2 
 
-sll s2, s2, 2 //Should output 0x8
+sll s2, s2, 2 #Should output 0x8
 
-srl s0, s2, 2 //should output 0x2
+srl s0, s2, 2 #should output 0x2
 
-and s3, s0, s2 //should output 0x0
+and s3, s0, s2 #should output 0x0
 
-or s3, s0, s2 //should output 0x0A
+or s3, s0, s2 #should output 0x0A
 
 slt 
 
@@ -20,17 +20,17 @@ sltu
 
 
 
-//Testing loading small immediate value:
+#Testing loading small immediate value:
 lui s0, 0xFF
-add    s0, zero, s0 //output 0xFF
+add    s0, zero, s0 #output 0xFF
 
-//Testing loading large immadede value
-//Method 1:
-li s0, 0xFEDC8EAB //testing load immediate 
-add    s0, zero, s0 //output 0xFEDC8EAB
-//Method 2:
+#Testing loading large immadede value
+#Method 1:
+li s0, 0xFEDC8EAB #testing load immediate 
+add    s0, zero, s0 #output 0xFEDC8EAB
+#Method 2:
 lui s0, 0xFEDC9
-addi s0, s0, 0xEAB //output 0xFEDC8EAB
+addi s0, s0, 0xEAB #output 0xFEDC8EAB
 
 
 
