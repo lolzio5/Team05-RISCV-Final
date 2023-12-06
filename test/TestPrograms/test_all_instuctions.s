fa0fa553b0080a8134b92ed28b21ac40640fd883
samuelwbarber <sbarber9876@outlook.com>
2023-12-06 10:58:10 +0000

    fixing error

--- test/TestPrograms/test_all_instuctions.s
+++ test/TestPrograms/test_all_instuctions.s
@@ -12,12 +12,9 @@ and s3, s0, s2 #should output 0x0
 
 or s3, s0, s2 #should output 0x0A
 
-slt 
+slt s3, s0, s2
 
-sltu
-
-
-
+sltu s3, s0, s2
 
 
 #Testing loading small immediate value:
@@ -31,6 +28,26 @@ add    s0, zero, s0 #output 0xFEDC8EAB
 #Method 2:
 lui s0, 0xFEDC9
 addi s0, s0, 0xEAB #output 0xFEDC8EAB
+
+#testing loop (meric)
+main:
+    addi    t0, zero, 10     # Initialize t0 with 10
+    addi    t1, zero, 0      # Initialize t1 with 0 (loop counter)
+
+loop:
+    addi    t1, t1, 1        # Increment t1
+    sll     t2, t1, 1        # Shift left logical t1 by 1, store in t2
+    xor     t3, t2, t1       # XOR t2 and t1, store in t3
+    bne     t1, t0, loop     # If t1 is not equal to t0, branch to loop
+
+    jal     ra, func         # Jump to func, store return address in ra
+
+func:
+    addi    t4, zero, 5      # Initialize t4 with 5
+    jalr    zero, ra, 0      # Return to the address in ra
+
+end:
+
 
 
 
