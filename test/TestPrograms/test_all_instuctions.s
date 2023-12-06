1b5182b6f39dbdb2372f2579b568e67c4946f070
samuelwbarber <sbarber9876@outlook.com>
2023-12-06 10:50:33 +0000

    Added test for all instuctions

---
+++ test/TestPrograms/test_all_instuctions.s
@@ -0,0 +1,35 @@
+//Testing basic arithmatic:
+li s0, 0xF1
+addi s1, s0, 0x1 //Should output 0xF2
+
+xor s2, s1, s0 //Should ouput 0x2 
+
+sll s2, s2, 2 //Should output 0x8
+
+srl s0, s2, 2 //should output 0x2
+
+slt 
+
+sltu 
+
+and 
+
+
+
+
+//Testing loading small immediate value:
+lui s0, 0xFF
+add    s0, zero, s0 //output 0xFF
+
+//Testing loading large immadede value
+//Method 1:
+li s0, 0xFEDC8EAB //testing load immediate 
+add    s0, zero, s0 //output 0xFEDC8EAB
+//Method 2:
+lui s0, 0xFEDC9
+addi s0, s0, 0xEAB //output 0xFEDC8EAB
+
+
+
+
+
