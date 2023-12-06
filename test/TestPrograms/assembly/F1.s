4e821348e195c65d19c202aba21a8729126fa478
lolzio5 <71973875+lolzio5@users.noreply.github.com>
2023-12-06 19:41:46 +0000

    fixed infinite loops and register naming convention

--- test/TestPrograms/assembly/F1.s
+++ test/TestPrograms/assembly/F1.s
@@ -1,41 +1,41 @@
 main:
     addi    a0, zero, 0x0                       # load a0 with 0 (a0 is vbd_value, the output)
-    addi    x2, zero, 0x0FF
-    addi    s2, zero, 0x000 
-    addi    s3, zero, 0x001
-    addi    s4, zero, 0x000
-    addi    s5, zero, 0x001
+    addi    t1, zero, 0x0FF
+    addi    a2, zero, 0x000 
+    addi    a3, zero, 0x001
+    addi    a4, zero, 0x000
+    addi    a5, zero, 0x001
     jal     x0, loop                            # Jump to loop
 
 loop:
-    addi    s1, zero, 0x00F                      # load s1 with a large number to be counted down
+    addi    t0, zero, 0x00F                     # load t0 with a large number to be counted down
     sll     a0, a0, 1                           # shift a0 by 1 so it goes up sequentially in decimal
     addi    a0, a0, 1                           # increment a0 by 1
-    beq     a0, x2, random_time                 # If a0 is 255, turn off after a random time
+    beq     a0, t1, random_time                 # If a0 is 255, turn off after a random time
     jal     x0, constant_time                   # Jump to constant_time
 
 constant_time:
-    addi    s1, s1, -1                          # Decrement s1
-    beq     s1, zero, loop                      # If s1 is equal to zero, return to loop
+    addi    t0, t0, -1                          # Decrement t0
+    beq     t0, zero, loop                      # If t0 is equal to zero, return to loop
     jal     x0, constant_time                   # Else continue looping
 
 random_time:
-    jal     x1, random_logic                    # Calculate the random value to be decremented                         # Make s6 much bigger so more time is wasted
-    addi    s6, s6, -1
-    beq     s6, zero, end                       # If s6 is equal to zero, end the program
-    jalr    x1, x1, 1                           # Else continue looping from the adding statement
+    jal     ra, random_logic                    # Calculate the random value to be decremented
+    addi    s1, s1, -1
+    beq     s1, zero, end                       # If s1 is equal to zero, end the program
+    jalr    x0, ra, 0                           # Else continue looping from the adding statement
 
 random_logic:
-    addi    s6, zero, 0x0
-    add     s6, s6, s2
-    sll     s3, s3, 0x001
-    add     s6, s6, s3
-    sll     s4, s4, 0x001
-    add     s6, s6, s4
-    sll     s5, s5, 0x001
-    add     s6, s6, s5
-    xor     s2, s4, s5
-    jalr    x0, x1, 1                           # Return to random_time, 1 instruction later
+    addi    s1, zero, 0x0
+    add     s1, s1, a2
+    sll     a3, a3, 0x001
+    add     s1, s1, a3
+    sll     a4, a4, 0x001
+    add     s1, s1, a4
+    sll     a5, a5, 0x001
+    add     s1, s1, a5
+    xor     a2, a4, a5
+    jalr    x0, ra, 0                           # Return to random_time, 1 instruction later
 
 end:
     addi    a0, zero, 0x0 
