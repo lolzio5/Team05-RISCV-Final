22ec37650d79200d8c823d1d040d94c7b88a7ff2
songmeric <jws119@ic.ac.uk>
2023-12-07 21:42:37 +0000

    This is the unit that decides what to forward to the ALU.

---
+++ rtl/HazardControl/AluOpForwarderE.sv
@@ -0,0 +1,14 @@
+module AluOpForwarderE(
+  input  logic [1:0]  iForwardAluOp1,
+  input  logic [1:0]  iForwardAluOp2,
+  input  logic [31:0] iResultDataW,
+  input  logic [31:0] iAluResultOutM,
+  input  logic [31:0] iRegData1E,
+  input  logic [31:0] iRegData2E,
+
+  output logic [31:0] oAluOp1,
+  output logic [31:0] oAluOp2,
+);
+
+
+endmodule
