821bae97d6b3668b563989e5dfed589851f2867c
n0sc3tipsum <da322@ic.ac.uk>
2023-12-09 16:06:17 +0400

    Updated PC Adder
    Added logic to output PC in the decode stage + 4 in the case of an incorrect branch decision (PC recovery)
    Changed jump offset for JALR (lsb is now only 1 bit of 0 rather than 2 bits of 0) may potentially resolve JALR bug

--- rtl/PC/PCAdderD.sv
+++ rtl/PC/PCAdderD.sv
@@ -1,31 +1,36 @@
 module PCAdderD(  
   input  InstructionTypes    iInstructionType,
   input  InstructionSubTypes iInstructionSubType,
-  input  logic [31:0]        iPC,
+  input  logic               iRecoverPCD,
+  input  logic [31:0]        iPCD,
   input  logic [31:0]        iImmExt,
   input  logic [31:0]        iRegOffset,
-
   output logic [31:0]        oPCTarget
 );
+
+  logic [31:0] jb_target;
 
   always_comb begin
     case(iInstructionType)
 
       JUMP : begin
         if (iInstructionSubType == JUMP_LINK_REG) begin
-          oPCTarget      = iImmExt + iRegOffset;
-          oPCTarget[1:0] = 2'b00;
+          jb_target      = iImmExt + iRegOffset;  //Potential RAW hazard
+          jb_target[0]   = 1'b0;
         end
 
         //No need to worry about alignment as iImmExt is already aligned for JAL and Branch instructions (from immdecode)
-        else    oPCTarget = iPC + iImmExt;  
+        else    jb_target = iPCD + iImmExt;  
       end
 
-      BRANCH  : oPCTarget = iPC + iImmExt;
+      BRANCH  : jb_target = iPCD + iImmExt;
 
-      default : oPCTarget = iPC + iImmExt;
+      default : jb_target = iPCD + iImmExt;
       
     endcase
+
+    oPCTarget = iRecoverPCD ? iPCD + 32'd4 : jb_target;
+
   end
 
 endmodule
