ab415e2c6e570f4bceb2aedc4314b7714ecccfdd
n0sc3tipsum <da322@ic.ac.uk>
2023-12-10 21:05:53 +0400

    Changed signal naming in ResultMuxW

--- rtl/Mux/ResultMuxW.sv
+++ rtl/Mux/ResultMuxW.sv
@@ -1,21 +1,21 @@
-module ResultMux(
-  input  logic [2:0]  iResultSrc,
-  input  logic [31:0] iMemDataOut,
-  input  logic [31:0] iAluResult,
-  input  logic [31:0] iPC,
-  input  logic [31:0] iUpperImm,
+module ResultMuxW(
+  input  logic [2:0]  iResultSrcW,
+  input  logic [31:0] iMemDataOutW,
+  input  logic [31:0] iAluResultW,
+  input  logic [31:0] iPCW,
+  input  logic [31:0] iUpperImmW,
 
-  output logic [31:0] oRegDataIn
+  output logic [31:0] oRegDataInW
 );
 
   always_comb begin
-    case(iResultSrc)
-      3'b000   : oRegDataIn = iAluResult;       // Alu Operation
-      3'b001   : oRegDataIn = iMemDataOut;      // Memory Read Operation (Load)
-      3'b010   : oRegDataIn = iPC + 32'd4;      // Jumps 
-      3'b011   : oRegDataIn = iUpperImm;        // Load Upper Immediate
-      3'b100   : oRegDataIn = iPC + iUpperImm;  // Add Upper Immediate To PC
-      default  : oRegDataIn = iAluResult;       // Default - Alu Operation
+    case(iResultSrcW)
+      3'b000   : oRegDataInW = iAluResultW;       // Alu Operation
+      3'b001   : oRegDataInW = iMemDataOutW;      // Memory Read Operation (Load)
+      3'b010   : oRegDataInW = iPCW + 32'd4;      // Jumps 
+      3'b011   : oRegDataInW = iUpperImmW;        // Load Upper Immediate
+      3'b100   : oRegDataInW = iPCW + iUpperImmW;  // Add Upper Immediate To PC
+      default  : oRegDataInW = iAluResultW;       // Default - Alu Operation
     endcase
   end
 
