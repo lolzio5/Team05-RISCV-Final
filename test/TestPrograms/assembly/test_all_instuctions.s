fcd8920f2da1768381202803864db935d4371d3d
songmeric <77728557+songmeric@users.noreply.github.com>
2023-12-10 18:15:41 +0000

    Update test_all_instuctions.s

--- test/TestPrograms/assembly/test_all_instuctions.s
+++ test/TestPrograms/assembly/test_all_instuctions.s
@@ -31,22 +31,25 @@ addi s0, s0, 0xEAB #output 0xFEDC8EAB
 
 #testing loop (meric)
 main:
-    addi    t0, zero, 10     # Initialize t0 with 10
-    addi    t1, zero, 0      # Initialize t1 with 0 (loop counter)
+    li    t0, 4    # Initialize t0 with 10
+    addi    t1, zero, 2      # Initialize t1 with 2 (loop counter)
 
 loop:
     addi    t1, t1, 1        # Increment t1
     sll     t2, t1, 1        # Shift left logical t1 by 1, store in t2
     xor     t3, t2, t1       # XOR t2 and t1, store in t3
     bne     t1, t0, loop     # If t1 is not equal to t0, branch to loop
+    jal     x1, func         # Jump to func, store return address in ra
 
-    jal     ra, func         # Jump to func, store return address in ra
+addi t1, t1, 100 #to confirm jalr
 
 func:
     addi    t4, zero, 5      # Initialize t4 with 5
-    jalr    zero, ra, 0      # Return to the address in ra
+    jalr    zero, x1, 0      # Return to the address in ra
 
 end:
+    # End of the program. Could include a halt instruction in a real scenario
+
 
 
 
