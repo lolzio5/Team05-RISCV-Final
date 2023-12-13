3dbd41481bd59e270d33982a5b39767664053f66
samuelwbarber <sbarber9876@outlook.com>
2023-12-13 12:25:09 +0000

    Adding Cache CacheDecode DataMemoryController and FindHit

---
+++ rtl/Memory/CacheDecode.sv
@@ -0,0 +1,18 @@
+module cache_decode (
+    input  logic [31:0] iAddress,
+    input logic [31:0] iFlushAddress,
+    output logic [3:0] oIndexFlush,
+    output logic [25:0] oTag,
+    output logic [3:0] oIndex
+);
+//////////////////////////////////////////////
+////       Decodes address and flush      ////
+//////////////////////////////////////////////
+always_comb begin
+
+    oTag <=iAddress[31:6];
+    oIndex <= iAddress[5:2];
+    oIndexFlush <= iFlushAddress[5:2];
+    
+end
+endmodule
---
+++ rtl/Memory/FindHit.sv
@@ -0,0 +1,18 @@
+module findhit (
+    input  logic iV,
+    input  logic [25:0]  iTagCache,
+    input  logic [25:0]  iTagTarget,
+    output logic  oHit
+);
+//////////////////////////////////////////////
+////            Hit logic                 ////
+//////////////////////////////////////////////
+always_comb begin
+
+    oHit=0;
+    if (iTagCache==iTagTarget&&iV==1) begin
+        oHit=1;
+    end
+
+end 
+endmodule
