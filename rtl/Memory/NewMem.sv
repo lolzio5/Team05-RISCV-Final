4e16e156224033d1527bb0016970c596d31128e9
samuelwbarber <124458984+samuelwbarber@users.noreply.github.com>
2023-12-15 18:24:30 +0000

    Update NewMem.sv

--- rtl/Memory/NewMem.sv
+++ rtl/Memory/NewMem.sv
@@ -32,201 +32,152 @@ module NewMem #(
     /* verilator lint_on UNUSED */
     
     initial begin
-        $readmemh("sine.hex", mem_array, 20'h10000);
+        $readmemh("triangle.mem", mem_array, 20'h10000);
     end
 
-    always_comb begin
+    always_comb begin        
+
+        word_aligned_address = {{iAddress[31:2]}, {2'b00}};                
+        byte_offset          = iAddress[1:0];       
         iTag =iAddress[31:6];
         iIndex = iAddress[5:2];
         cTag = tag_cache_array[iIndex];
         cV = valid_cache_array[iIndex];
         cData = data_cache_array[iIndex];
-        word_aligned_address = {{iAddress[31:2]}, {2'b00}};
-        byte_offset          = iAddress[1:0];     
+        byte4 =   mem_array[word_aligned_address + 32'd3 ][7:0];
+        byte3 =   mem_array[word_aligned_address + 32'd2][7:0];
+        byte2 =   mem_array[word_aligned_address + 32'd1][7:0];
+        byte1 =   mem_array[word_aligned_address][7:0];
+        case (iInstructionType) 
+            STORE : begin
+                tag_cache_array[iIndex]=0;
+                case (iMemoryInstructionType)
+
+                    STORE_BYTE : begin
+                        case (byte_offset) 
+                            2'b00 : byte1 = iMemData[7:0];
+                            2'b01 : byte2 = iMemData[7:0];
+                            2'b10 : byte3 = iMemData[7:0];
+                            2'b11 : byte4 = iMemData[7:0];
+                        endcase
+                    end
+                    
+                    
+                    STORE_HALF : begin
+                        case (byte_offset) 
+                            2'b00 : begin 
+                                byte1 = iMemData[7:0];
+                                byte2 = iMemData[15:8];
+                            end
+
+                            2'b01 : begin
+                                byte2 = iMemData[7:0];
+                                byte3 = iMemData[15:8];
+                            end
+                            2'b10 : begin
+                                byte3 = iMemData[7:0];
+                                byte4 = iMemData[15:8];
+                            end
+                            2'b11 : begin
+                                byte3 = iMemData[7:0];
+                                byte4 = iMemData[15:8];
+                            end
+                        endcase
+                    end
+                    
+                    STORE_WORD : begin
+                        byte4 = iMemData[31:24];
+                        byte3 = iMemData[23:16];
+                        byte2 = iMemData[15:8];
+                        byte1 = iMemData[7:0];
+                    end
+
+                    default    : begin
+                        byte4 = iMemData[31:24];
+                        byte3 = iMemData[23:16];
+                        byte2 = iMemData[15:8];
+                        byte1 = iMemData[7:0];
+                    end
+
+                endcase
+            end
+
+            LOAD : begin 
+                if (cTag==iTag&&cV==1) begin //hit
+                    byte4 =   cData[31:24];
+                    byte3 =   cData[23:16];
+                    byte2 =   cData[15:8];
+                    byte1 =   cData[7:0];
+                end
+                else begin //miss
+                    tag_cache_array[iIndex]=iTag;
+                    data_cache_array[iIndex]={byte4, byte3, byte2, byte1};
+                    valid_cache_array[iIndex]=1;
+                end 
+                case(iMemoryInstructionType)
+
+                    LOAD_BYTE  : begin
+
+                        case (byte_offset) 
+                            2'b00 : oMemData[7:0] = byte1;
+                            2'b01 : oMemData[7:0] = byte2;
+                            2'b10 : oMemData[7:0] = byte3;
+                            2'b11 : oMemData[7:0] = byte4;
+                        endcase
+
+                        oMemData[31:8] = {24{oMemData[7]}}; //sign extend
+                    end
+
+                    LOAD_HALF  : begin
+
+                        case (byte_offset) 
+                            2'b00 : oMemData[15:0] = {byte2, byte1};
+                            2'b01 : oMemData[15:0] = {byte3, byte2};
+                            2'b10 : oMemData[15:0] = {byte4, byte3};
+                            2'b11 : oMemData[15:0] = {byte4, byte3};
+                        endcase
+                        
+                        oMemData[31:16] = {16{oMemData[15]}}; //sign extend
+                    end
+
+                    ULOAD_BYTE : begin            
+
+                        case (byte_offset) 
+                            2'b00 : oMemData[7:0] = byte1;
+                            2'b01 : oMemData[7:0] = byte2;
+                            2'b10 : oMemData[7:0] = byte3;
+                            2'b11 : oMemData[7:0] = byte4;
+                        endcase
+
+                        oMemData[31:8] = {24{1'b0}}; //zero extend
+
+                    end
+
+                    ULOAD_HALF : begin
+
+                        case (byte_offset) 
+                            2'b00 : oMemData[15:0] = {byte2, byte1};
+                            2'b01 : oMemData[15:0] = {byte3, byte2};
+                            2'b10 : oMemData[15:0] = {byte4, byte3};
+                            2'b11 : oMemData[15:0] = {byte4, byte3};
+                        endcase
+
+                        oMemData[31:16] = {16{1'b0}}; //Zero extend
+
+                    end
+
+                    LOAD_WORD  : oMemData = {byte4, byte3, byte2, byte1};               
+
+                    default    : oMemData = {byte4, byte3, byte2, byte1};
+                endcase
+            end
+
+            default : oMemData = {byte4, byte3, byte2, byte1};
+
+        endcase
     end
 
-    always_comb begin
-        if(iWriteEn==0&&(cTag==iTag)&&cV==1)begin //Hit
-            byte4 =   cData[31:24];
-            byte3 =   cData[23:16];
-            byte2 =   cData[15:8];
-            byte1 =   cData[7:0];
-            case(iMemoryInstructionType)
-                LOAD_BYTE  : begin
-                    case (byte_offset) 
-                        2'b00 : oMemData[7:0] = byte1;
-                        2'b01 : oMemData[7:0] = byte2;
-                        2'b10 : oMemData[7:0] = byte3;
-                        2'b11 : oMemData[7:0] = byte4;
-                    endcase
-
-                    oMemData[31:8] = {24{oMemData[7]}}; //sign extend
-                end
-
-                LOAD_HALF  : begin
-
-                    case (byte_offset) 
-                        2'b00 : oMemData[15:0] = {byte2, byte1};
-                        2'b01 : oMemData[15:0] = {byte3, byte2};
-                        2'b10 : oMemData[15:0] = {byte4, byte3};
-                        2'b11 : oMemData[15:0] = {byte4, byte3};
-                    endcase
-                            
-                    oMemData[31:16] = {16{oMemData[15]}}; //sign extend
-                end
-
-                ULOAD_BYTE : begin            
-
-                    case (byte_offset) 
-                        2'b00 : oMemData[7:0] = byte1;
-                        2'b01 : oMemData[7:0] = byte2;
-                        2'b10 : oMemData[7:0] = byte3;
-                        2'b11 : oMemData[7:0] = byte4;
-                    endcase
-
-                    oMemData[31:8] = {24{1'b0}}; //zero extend
-
-                end
-
-                ULOAD_HALF : begin
-
-                    case (byte_offset) 
-                        2'b00 : oMemData[15:0] = {byte2, byte1};
-                        2'b01 : oMemData[15:0] = {byte3, byte2};
-                        2'b10 : oMemData[15:0] = {byte4, byte3};
-                        2'b11 : oMemData[15:0] = {byte4, byte3};
-                    endcase
-
-                    oMemData[31:16] = {16{1'b0}}; //Zero extend
-
-                end
-
-                LOAD_WORD  : begin
-                    oMemData = {byte4, byte3, byte2, byte1};             
-                end
-                default    : begin 
-                    oMemData = {byte4, byte3, byte2, byte1};
-                end
-            endcase
-        end
-        else if (iWriteEn==0) begin//Miss
-            byte4 =   mem_array[word_aligned_address + 32'd3 ][7:0];
-            byte3 =   mem_array[word_aligned_address + 32'd2][7:0];
-            byte2 =   mem_array[word_aligned_address + 32'd1][7:0];
-            byte1 =   mem_array[word_aligned_address][7:0];
-            case(iMemoryInstructionType)
-                LOAD_BYTE  : begin
-                    case (byte_offset) 
-                        2'b00 : oMemData[7:0] = byte1;
-                        2'b01 : oMemData[7:0] = byte2;
-                        2'b10 : oMemData[7:0] = byte3;
-                        2'b11 : oMemData[7:0] = byte4;
-                    endcase
-
-                    oMemData[31:8] = {24{oMemData[7]}}; //sign extend
-                end
-
-                LOAD_HALF  : begin
-
-                    case (byte_offset) 
-                        2'b00 : oMemData[15:0] = {byte2, byte1};
-                        2'b01 : oMemData[15:0] = {byte3, byte2};
-                        2'b10 : oMemData[15:0] = {byte4, byte3};
-                        2'b11 : oMemData[15:0] = {byte4, byte3};
-                    endcase
-                            
-                    oMemData[31:16] = {16{oMemData[15]}}; //sign extend
-                end
-
-                ULOAD_BYTE : begin            
-
-                    case (byte_offset) 
-                        2'b00 : oMemData[7:0] = byte1;
-                        2'b01 : oMemData[7:0] = byte2;
-                        2'b10 : oMemData[7:0] = byte3;
-                        2'b11 : oMemData[7:0] = byte4;
-                    endcase
-
-                    oMemData[31:8] = {24{1'b0}}; //zero extend
-
-                end
-
-                ULOAD_HALF : begin
-
-                    case (byte_offset) 
-                        2'b00 : oMemData[15:0] = {byte2, byte1};
-                        2'b01 : oMemData[15:0] = {byte3, byte2};
-                        2'b10 : oMemData[15:0] = {byte4, byte3};
-                        2'b11 : oMemData[15:0] = {byte4, byte3};
-                    endcase
-
-                    oMemData[31:16] = {16{1'b0}}; //Zero extend
-
-                end
-
-                LOAD_WORD  : begin
-                    oMemData = {byte4, byte3, byte2, byte1};             
-                end
-                default    : begin 
-                    oMemData = {byte4, byte3, byte2, byte1};
-                end
-            endcase
-            tag_cache_array[iIndex]=iTag;
-            valid_cache_array[iIndex]=1;
-            data_cache_array[iIndex]={byte4, byte3, byte2, byte1};
-        end
-        else begin //Write
-            case (iMemoryInstructionType)
-                STORE_BYTE : begin
-                    case (byte_offset) 
-                        2'b00 : byte1 = iMemData[7:0];
-                        2'b01 : byte2 = iMemData[7:0];
-                        2'b10 : byte3 = iMemData[7:0];
-                        2'b11 : byte4 = iMemData[7:0];
-                    endcase
-                end
-                
-                
-                STORE_HALF : begin
-                    case (byte_offset) 
-                        2'b00 : begin 
-                            byte1 = iMemData[7:0];
-                            byte2 = iMemData[15:8];
-                        end
-
-                        2'b01 : begin
-                            byte2 = iMemData[7:0];
-                            byte3 = iMemData[15:8];
-                        end
-                        2'b10 : begin
-                            byte3 = iMemData[7:0];
-                            byte4 = iMemData[15:8];
-                        end
-                        2'b11 : begin
-                            byte3 = iMemData[7:0];
-                            byte4 = iMemData[15:8];
-                        end
-                    endcase
-                end
-                
-                STORE_WORD : begin
-                    byte4 = iMemData[31:24];
-                    byte3 = iMemData[23:16];
-                    byte2 = iMemData[15:8];
-                    byte1 = iMemData[7:0];
-                end
-
-                default    : begin
-                    byte4 = iMemData[31:24];
-                    byte3 = iMemData[23:16];
-                    byte2 = iMemData[15:8];
-                    byte1 = iMemData[7:0];
-                end
-            endcase
-        end
-    end
-
-    always_ff @(posedge iClk) begin
+    always_ff @(negedge iClk) begin
         if (iWriteEn) begin
             mem_array[word_aligned_address + 32'd3][7:0] <= byte4;
             mem_array[word_aligned_address + 32'd2][7:0] <= byte3;
@@ -234,8 +185,12 @@ module NewMem #(
             mem_array[word_aligned_address][7:0]         <= byte1;
             valid_cache_array[iIndex]<=0;
         end
-        else             ooMemData                        <= oMemData;
+        else begin
+            ooMemData                        <= oMemData;
+        end
     end
 
 endmodule
+
+
 
